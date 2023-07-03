7b9a94dc4167eba4786cf9fa219a32af3a5b85cc
Mithu Bortamuly <30743120+MithuBitz@users.noreply.github.com>
2023-07-03 15:01:18 +0530

    The first commit

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,7 @@
+const path = require(`path`)
+// import { createFilePath } from "gatsby-source-filesystem"
+const { createFilePath } = require(`gatsby-source-filesystem`)
+
 /**
  * Implement Gatsby's Node APIs in this file.
  *
@@ -7,12 +11,55 @@
 /**
  * @type {import('gatsby').GatsbyNode['createPages']}
  */
-exports.createPages = async ({ actions }) => {
+// exports.createPages = async ({ actions }) => {
+//   const { createPage } = actions
+//   createPage({
+//     path: "/using-dsg",
+//     component: require.resolve("./src/templates/using-dsg.js"),
+//     context: {},
+//     defer: true,
+//   })
+// }
+
+
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions
+  if(node.internal.type === `MarkdownRemark`) {
+    const slug = createFilePath({ node, getNode })
+
+    createNodeField({
+      node, 
+      name: `slug`,
+      value: slug
+    })
+  }
+}
+
+
+exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions
-  createPage({
-    path: "/using-dsg",
-    component: require.resolve("./src/templates/using-dsg.js"),
-    context: {},
-    defer: true,
+
+  return graphql(`
+    {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+             slug
+            }
+          }
+        }
+      }
+    }
+  `).then(result => {
+    result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+      createPage({
+        path: node.fields.slug,
+        component: path.resolve(`src/templates/blog-post.js`),
+        context: {
+          slug: node.fields.slug
+        }
+      })
+    })
   })
 }
---
+++ src/pages/page-3.js
@@ -0,0 +1,14 @@
+import * as React from "react"
+
+
+import Layout from "../components/layout"
+
+
+const ThiredPage = () => (
+  <Layout>
+    <h1>Wellcome from Thired Page</h1>  
+    
+  </Layout>
+)
+
+export default ThiredPage
